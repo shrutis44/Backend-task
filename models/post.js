fe951256c96e90c1bda73254f1d1a77ae05aebe9
shrutis44 <shrutimaurya782@gamil.com>
2024-11-07 23:02:06 +0530

    init commit

--- models/post.js
+++ models/post.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { 
+        type: String,
+        required: true 
+    },
     image: String,
-    description: { type: String, required: true },
+    description: { 
+        type: String, 
+        required: true 
+    },
     tags: [String],
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     createdAt: { type: Date, default: Date.now },
